b764e24eee866a43eeb34797bd67a7fa8daa2661
alastair-simon <alastairfraser.work@gmail.com>
2024-11-23 15:11:52 +0100

    add vitest

--- component-library/vite.config.ts
+++ component-library/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: ['./tsconfig.vitest.json']
+    })
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html']
+    },
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+    typecheck: {
+      tsconfig: './tsconfig.vitest.json'
+    }
+  }
 });
